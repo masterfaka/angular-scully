5b18bbef3549a996bef648d940a560f2d770de3a
robert-edward-friedel <robert-edward.friedel@hpe.com>
2023-03-02 21:42:15 +0100

    change for demo

--- src/app/crisis-center/mock-crises.ts
+++ src/app/crisis-center/mock-crises.ts
@@ -1,7 +1,7 @@
 import { Crisis } from './crisis';
 
 export const CRISES: Crisis[] = [
-  { id: 1, name: 'Dragons Burning Cities' },
+  { id: 1, name: 'Dragons Burning Cities some changes' },
   { id: 2, name: 'Sky Rains Great White Sharks' },
   { id: 4, name: 'Procrastinators Meeting Delayed Again' },
 ];
